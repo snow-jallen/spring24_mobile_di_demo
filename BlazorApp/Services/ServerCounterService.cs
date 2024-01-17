b74f8674dd8ca4a43e2ac089aefd4966d8e34716
Jonathan Allen <jonathan.allen@snow.edu>
2024-01-17 15:41:23 -0700

    Add project files.

---
+++ BlazorApp/Services/ServerCounterService.cs
@@ -0,0 +1,18 @@
+﻿using RazorClassLibrary1.Services;
+
+namespace BlazorApp.Services
+{
+    public class ServerCounterService : ICounterService
+    {
+        private int Count = 0;
+        public Task<int> GetCurrentValue()
+        {
+            return Task.FromResult(Count);
+        }
+
+        public Task<int> IncrementCurrentValue()
+        {
+            return Task.FromResult(++Count);
+        }
+    }
+}
---
+++ RazorClassLibrary1/Services/ICounterService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary1.Services
+{
+    public interface ICounterService
+    {
+        Task<int> GetCurrentValue();
+        Task<int> IncrementCurrentValue();
+    }
+}
